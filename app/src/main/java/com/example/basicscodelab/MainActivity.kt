ad5c4a5254459cb51d13105bd493bb63c0608d07
Christina A. Upshaw <chrisaupshaw@gmail.com>
2024-09-09 12:34:49 -0700

    Successfully draws "terrain" as pixels. Next step is allowing scrolling.

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,30 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.coerceAtLeast
+import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+import kotlin.math.max
+import kotlin.math.min
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,116 +29,55 @@ class MainActivity : ComponentActivity() {
             }
     }
 }
+
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,
-    names: List<String> = List(1000){"$it"}
+    terrain: Array<IntArray> = mkNewTerrain(1024)
 ) {
-    val shouldShowOnboarding = rememberSaveable { mutableStateOf(true) }
-
-    if (shouldShowOnboarding.value) {
-        OnboardingScreen(modifier = modifier, onContinueClicked = {shouldShowOnboarding.value = false})
-    } else {
-        GreetingList(modifier = modifier, names = names)
+    val x = rememberSaveable() {
+        mutableFloatStateOf(0f)
     }
-}
+    val y = rememberSaveable() {
+        mutableFloatStateOf(0f)
+    }
+    val boxSize = Size (200f, 200f)
+    Canvas (modifier = modifier.fillMaxSize()) {
+        val rowWidth = size.width / boxSize.width
+        val colHeight = size.height / boxSize.height
 
-@Composable
-fun GreetingList(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
-) {
-    Surface(modifier = Modifier.padding(vertical = 10.dp)) {
-        LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-            items(items = names) { name ->
-                Greeting(name = name)
+        val startX : Int = 0 + x.floatValue.toInt()
+        val endX : Int = (rowWidth + x.floatValue).toInt() + 1
+        val startY : Int = 0 + y.floatValue.toInt()
+        val endY : Int = (colHeight + y.floatValue).toInt() + 1
+
+        for (i in (startX .. endX))
+            for (j in (startY .. endY)) {
+                drawRect(
+                    color = altitudeToColor(terrain[i][j]),
+                    topLeft = Offset(
+                        i.toFloat()*boxSize.width - x.floatValue,
+                        j.toFloat()*boxSize.height - y.floatValue),
+                    size = boxSize
+                    )
             }
-        }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    val expanded = rememberSaveable {
-        mutableStateOf(false)
-    }
-    val extraPadding = animateDpAsState (
-        if (expanded.value) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioLowBouncy,
-            stiffness = Spring.StiffnessLow
-        )
+private fun altitudeToColor(altitude : Int): Color {
+    val nonGreen : Int = 2 * max((altitude - 128), 0)
+    val green : Int = 2 * min (255, altitude + 128)
+    return Color(
+        green = green,
+        red = nonGreen,
+        blue = nonGreen
     )
+}
 
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)) {
-                Text(text = "Hello ")
-                Text(modifier = Modifier.padding(5.dp), text = name)
-                Canvas(modifier = Modifier
-                    .fillMaxSize()
-                    .height(extraPadding.value.coerceAtLeast(0.dp))) {
-                    val canvasQuadrantSize = size / 2F
-                    drawRect(
-                        color = Color.Magenta,
-                        size = canvasQuadrantSize
-                    )
-                    drawLine(
-                        start = Offset(x = size.width, y = 0f),
-                        end = Offset(x = 0f, y = size.height),
-                        color = Color.White
-                    )
-                }
-            }
-            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
-                Text(if (expanded.value) "Show less" else "Show more")
-            }
+fun mkNewTerrain(size : Int): Array<IntArray> {
+    return Array(size) {
+        IntArray (size) {
+            (0..255).random()
         }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BasicsCodelabTheme {
-        Greeting("There")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun AppPreview() {
-    MyApp(names = listOf("Preview", "Test", "QA"))
-}
-
-
-@Composable
-fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Welcome to the Basics Codelab!")
-        ElevatedButton(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
-        ) {
-            Text("Continue")
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-fun OnboardingPreview() {
-    BasicsCodelabTheme {
-        OnboardingScreen(onContinueClicked = {})
     }
 }
